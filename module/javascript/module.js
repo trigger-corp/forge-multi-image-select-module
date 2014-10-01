030656d6bd5093ceabbfa8cccdc5b1a94d156b33
Antoine van Gelder <antoine@trigger.io>
2014-10-01 14:09:41 +0200

    Fix module.js method

--- module/javascript/module.js
+++ module/javascript/module.js
@@ -1,6 +1,6 @@
 // Expose the native API to javascript
 forge.tribr_multi_image_select = {
-    getImages: function (text, success, error) {
+    getImages: function (success, error) {
         forge.internal.call('tribr_multi_image_select.getImages', {}, success, error);
     }
 };
