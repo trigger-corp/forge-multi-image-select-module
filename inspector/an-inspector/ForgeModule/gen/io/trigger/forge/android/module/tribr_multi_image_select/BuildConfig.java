5123a8eccd12b43e4ab669938b9c2dd00091d63b
Antoine van Gelder <antoine@trigger.io>
2014-10-01 12:07:51 +0200

    Cleanz

--- inspector/an-inspector/ForgeModule/gen/io/trigger/forge/android/module/tribr_multi_image_select/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package io.trigger.forge.android.module.tribr_multi_image_select;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
