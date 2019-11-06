3e1520c160767b5b7ce430f59544d7f87892edd9
Antoine van Gelder <antoine@trigger.io>
2019-11-06 17:11:15 +0200

    Add test for module against file and request modules

--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -2,6 +2,31 @@
 
 module("gallery");
 
+if (forge.file && forge.request) {
+    var upload_url = "https://httpbin.org/post";
+
+    asyncTest("gallery.getImages upload", 1, function() {
+        forge.gallery.getImages(function (files) {
+            forge.logging.log("getImages returned: " + JSON.stringify(files));
+            forge.request.ajax({
+                url: upload_url,
+                files: files,
+                success: function (data) {
+                    forge.logging.log("RECEIVED RESPONSE: " + JSON.stringify(data));
+                    ok(true, "Success");
+                    start();
+                },
+                error: function (e) {
+                    ok(false, "forge.request.ajax failed: " + JSON.stringify(e));
+                    start();
+                }
+            });
+        });
+    });
+
+}
+
+
 asyncTest("Attempt to select images", 1, function() {
     forge.gallery.getImages(function (images) {
         askQuestion("Are these the images you selected: " + JSON.stringify(images), {
